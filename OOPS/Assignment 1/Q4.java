abf2f3e1d7d14ad2b9cab534fdb72a4dbaca23a5
Gauravahirra <gauravahirrao75@gmail.com>
2025-03-06 23:22:55 +0530

    Add files via upload

---
+++ OOPS/Assignment 1/Q4.java
@@ -0,0 +1,17 @@
+
+public class Q4{
+	public static void main(String args[])
+	{
+		//wrapper class Int to double & vice versa
+		Integer i = 10;
+		Double d =  i.doubleValue();
+		System.out.println("Integer i = "+ i);
+		System.out.println("Double d = "+ d);
+		System.out.println("---------------------");
+		//Double to int
+		Double d1 = 13.233;
+		Integer b1 = d1.intValue();
+		System.out.println("Double d1 = "+ d1);
+		System.out.println("Integer b1 = " + b1);
+		}
+}
